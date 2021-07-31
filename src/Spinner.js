6529e8eec66646b08e251a4198c4c1ef099f4000
egiegarciano <egiegarciano14@gmail.com>
2021-07-31 19:32:27 +0800

    Section3-6

---
+++ src/Spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SeasonDisplay.css'
+
+const Spinner = (props) => {
+    return (
+        <div className="ui active dimmer border">
+            <div className="ui big text loader">
+                {props.message}
+            </div>
+        </div>
+    );
+};
+
+Spinner.defaultProps = {
+    message: 'Loading...'
+};
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './SeasonDisplay.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message})
+            
+        );
+    }
+
+    renderContent() {
+        // Make a separate component for the error, just for practice
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        } 
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please allow location request"/>;
+    }
+
+    // React says we have to define render!!
+    render () {
+        return (
+            <div className="border">
+                {this.renderContent()}
+            </div>
+        );
+    }
+
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
